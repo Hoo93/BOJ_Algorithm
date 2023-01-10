908db628187c854bf0353a033f006ee461b04c68
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-10 13:59:06 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,8 @@
+import sys
+import itertools
+
+n,m = map(int,sys.stdin.readline().rstrip().rsplit())
+nums = [str(i) for i in range(1,n+1)]
+permut = itertools.combinations(nums,m)
+
+print("\n".join(" ".join(comb) for comb in permut))
