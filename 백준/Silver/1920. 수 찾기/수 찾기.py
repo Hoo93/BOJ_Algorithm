2b951ed39ceda80cc82d8ac069ff8f403b6519e7
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-13 02:35:36 +0900

    [Silver IV] Title: 수 찾기, Time: 124 ms, Memory: 49428 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,17 +1,16 @@
 import sys
-import bisect
 
 input = sys.stdin.readline
 
 input()
-compare_list = sorted(map(int,input().strip().split()))
+compare_list = set(map(int,input().strip().split()))
 input()
 number_list = list(map(int,input().strip().split()))
 
 answer = []
 
 for num in number_list:
-    if compare_list[bisect.bisect_right(compare_list,num)-1] == num:
+    if num in compare_list:
         answer.append("1")
     else:
         answer.append("0")
