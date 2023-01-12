7d18b5288ebdd4e832414b85c1360c0638e6df01
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-13 01:55:04 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+dp = [[0,0,0,0,0,0,0,0,0,0],[0,1,1,1,1,1,1,1,1,1]] +[ [0,0,0,0,0,0,0,0,0,0] for _ in range(100)]
+
+for i in range(2,n+1):
+    dp[i][0] = dp[i-1][1]
+    dp[i][9] = dp[i-1][8]
+    for j in range(1,9):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[n])%1000000000)
