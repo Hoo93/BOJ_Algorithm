8c65aa2b93f9282cb5155d587ec121fec4159e20
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-14 02:31:23 +0900

    [Silver II] Title: 나무 자르기, Time: 668 ms, Memory: 262752 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -9,7 +9,7 @@ left,right = 0,2000000000
 
 while left <= right:
     mid = (left+right)//2
-    total_length = sum([num-mid for num in number_list if num >= mid])
+    total_length = sum(num-mid for num in number_list if num >= mid)
     if total_length < k:
         right = mid -1
     elif total_length >= k:
