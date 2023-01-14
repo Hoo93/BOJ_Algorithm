c8b2e4fa6319faf101b6d2c91a39db79ce9e9453
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-14 12:46:07 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -7,11 +7,12 @@ word = input().rstrip()
 dic = {}
 answer = 0
 hash = 1
+
 for i in range(97,123):
     dic[chr(i)] = i-96
 
 for i in range(len(word)):
-    answer += (dic[word[i]]*hash)%1234567891
+    answer += (dic[word[i]]*(hash%1234567891))%1234567891
     hash *= 31
 
 print(answer)
