f742ee379c8636077393c8992ddb3a6b49122612
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-17 15:48:25 +0900

    [Gold V] Title: 평범한 배낭, Time: 4692 ms, Memory: 34336 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int,input().rstrip().split())
+stuff = [(0,0)]+[tuple(map(int,input().rstrip().rsplit())) for _ in range(n) ]
+
+dp = [0 for _ in range(k+1)]
+
+for i in range(1,n+1):
+    for j in range(k,0,-1):
+        weight = stuff[i][0]
+        value  = stuff[i][1]
+
+        if weight > j:
+            break
+        else:
+            dp[j] = max(dp[j],dp[j-weight]+value)
+
+print(dp[k])
