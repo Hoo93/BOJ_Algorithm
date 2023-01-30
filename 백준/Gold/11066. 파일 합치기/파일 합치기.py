fdc87fd214106291867bb01392c246afd205bf9f
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-30 16:02:43 +0900

    [Gold III] Title: 파일 합치기, Time: 220 ms, Memory: 131864 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -17,19 +17,25 @@ for _ in range(test_num):
     
     for i in range(1,number):
         dp[i][i+1] = psum[i+1]-psum[i-1]
+
     
+    # optimization array / 초기값 설정
     for i in range(1,number+1):
         opt[i][i] = i
     
-    for i in range(number-1,0,-1):
-        for j in range(i+1,number+1):
-            cost = psum[j] - psum[i-1]
+    for start in range(number-1,0,-1):
+        for end in range(start+1,number+1):
+            cost = psum[end] - psum[start-1]
             minimum = mx
-            for k in range(opt[i][j-1],min(j,opt[i+1][j]+1)):
-                tmp = dp[i][k]+dp[k+1][j]
+            # Knuth Optimization 을 사용해 mid 의 범위를 축소 시킴 !!!!!
+            # mid의 값은 end 보다는 작아야 하므로 min()을 사용해줌
+            for mid in range(opt[start][end-1],min(end,opt[start+1][end]+1)):
+                tmp = dp[start][mid]+dp[mid+1][end]
+
+                # 최단 경로의 값과 그 때의 중간 경로 지점을 opt에 저장
                 if tmp <= minimum:
                     minimum = tmp
-                    opt[i][j] = k
-            dp[i][j] = cost + minimum
+                    opt[start][end] = mid
+            dp[start][end] = cost + minimum
 
     print(dp[1][number])
