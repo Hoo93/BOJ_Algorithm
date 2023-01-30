81ac3ff2bbe6022372be9292fccf0b6d5c812cb2
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-01-30 15:03:42 +0900

    [Gold III] Title: 파일 합치기, Time: 224 ms, Memory: 131464 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -2,30 +2,34 @@ import sys
 
 input = sys.stdin.readline
 test_num = int(input().rstrip())
+mx = 1e8
 for _ in range(test_num):
     number = int(input().rstrip())
     
     num_list = [0] + list(map(int,input().rstrip().split()))
-    psum = [[0 for _ in range(number+1)] for _ in range(number+1)]
+    psum = [0 for _ in range(number+1)]
     
     for i in range(1,number+1):
-        tmp = 0
-        for j in range(i,number+1):
-            tmp += num_list[j]
-            psum[i][j] = tmp
-    
+        psum[i] = psum[i-1] + num_list[i]
+
     dp = [[0 for _ in range(number+1)] for _ in range(number+1)]
+    opt = [[0 for _ in range(number+1)] for _ in range(number+1)]
     
     for i in range(1,number):
-        dp[i][i+1] = psum[1][i+1]-psum[1][i-1]
+        dp[i][i+1] = psum[i+1]-psum[i-1]
     
-    for i in range(2,number):
-        for j in range(1,number+1-i):
-            minimum = dp[j][j] + dp[j+1][j+i]
-            for k in range(j+1,j+i):
-                tmp = dp[j][k]+dp[k+1][j+i]
-                if tmp < minimum:
+    for i in range(1,number+1):
+        opt[i][i] = i
+    
+    for i in range(number-1,0,-1):
+        for j in range(i+1,number+1):
+            cost = psum[j] - psum[i-1]
+            minimum = mx
+            for k in range(opt[i][j-1],min(j,opt[i+1][j]+1)):
+                tmp = dp[i][k]+dp[k+1][j]
+                if tmp <= minimum:
                     minimum = tmp
-            dp[j][j+i] = psum[j][j+i] + minimum
-    
+                    opt[i][j] = k
+            dp[i][j] = cost + minimum
+
     print(dp[1][number])
