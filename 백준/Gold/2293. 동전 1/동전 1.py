685ecde6936446d79f5af71455d2b1c8f42a93fb
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-02-13 16:51:31 +0900

    [Gold V] Title: 동전 1, Time: 200 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,20 @@
+import sys
+
+n,k = map(int,sys.stdin.readline().rstrip().split())
+
+coins = []
+
+for _ in range(n):
+    coins.append(int(sys.stdin.readline().rstrip()))
+
+coins.sort()
+
+dp = [ 0 for _ in range(k+1)]
+dp[0] = 1
+for i in range(1,k//coins[0]+1):
+    dp[i*coins[0]] = 1
+for i in range(1,n):
+    for j in range(coins[i],k+1):
+        dp[j] += dp[j-coins[i]]
+
+print(dp[k])
