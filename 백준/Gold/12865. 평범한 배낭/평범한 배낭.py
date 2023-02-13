df2f66a5ffca765eeaaf194e8075db17fd2485bd
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-02-13 18:00:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 115272 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -8,9 +8,10 @@ stuff = [(0,0)]+[tuple(map(int,input().rstrip().rsplit())) for _ in range(n) ]
 dp = [0 for _ in range(k+1)]
 
 for i in range(1,n+1):
+    weight = stuff[i][0]
+    value  = stuff[i][1]
     for j in range(k,0,-1):
-        weight = stuff[i][0]
-        value  = stuff[i][1]
+        
 
         if weight > j:
             break
