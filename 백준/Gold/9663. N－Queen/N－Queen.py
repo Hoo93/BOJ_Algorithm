5230244a7d7deeed23360824b9d3714baeed92e6
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-02-15 01:14:41 +0900

    [Gold IV] Title: N-Queen, Time: 16456 ms, Memory: 160856 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,24 +1,32 @@
 import sys
+
+depth = int(sys.stdin.readline().rstrip())
+
+row = [0 for _ in range(depth)]
 cnt = 0
- 
-def is_promissing(floor:int)->bool:
-    for index in range(floor):
-        if row[index]==row[floor] or abs(row[index]-row[floor]) == floor - index:
+
+# is_possisble col을 대입하면 현재 row와 비교해 가능한지 출력
+def check(row,col):
+    length = len(row)
+    if col in row:
+        return False
+    for i in range(len(row)):
+        if row[length-i-1] == col - i-1:
+            return False
+        elif row[length-i-1] == col + i+1:
             return False
     return True
 
-def n_queen(floor:int):
+def nqueen(row:list,floor):
     global cnt
-    if floor == depth:
+    if floor == depth-1:
         cnt += 1
     else:
-        for j in range(depth):
-            row[floor] = j
-            if is_promissing(floor):
-                n_queen(floor+1)
+        for i in range(depth):
+            if check(row,i):
+                nqueen(row+[i],floor+1)
+            
+for i in range(depth):
+    nqueen([i],0)
 
-depth =int(sys.stdin.readline().strip())
-row = [0]*depth
-n_queen(0)
 print(cnt)
-    
