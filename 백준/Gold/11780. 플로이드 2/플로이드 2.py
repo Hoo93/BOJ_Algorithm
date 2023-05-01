96160a3a2cf1125a20c936a723e6e6b81bcf7fe0
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-05-01 12:05:35 +0900

    [Gold II] Title: 플로이드 2, Time: 428 ms, Memory: 42060 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.py
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.py
@@ -9,6 +9,8 @@ buses = int(instructions[1].rstrip())
 
 visited = [[-1 for _ in range(cities + 1)] for _ in range(cities + 1)]
 costs = [[200000 for _ in range(cities + 1)] for _ in range(cities + 1)]
+paths = dict()
+path_set = set()
 
 for i in range(cities + 1):
     costs[i][i] = 0
@@ -39,12 +41,19 @@ for i in costs[1:]:
 def getPath(depart, arrive):
     if visited[depart][arrive] == -1:
         return [0]
+    if (depart, arrive) in path_set:
+        return paths[(depart, arrive)]
     else:
         stopby = visited[depart][arrive]
         if stopby == depart:
+            path_set.add((depart, arrive))
+            paths[(depart, arrive)] = [depart]
             return [depart]
         else:
-            return getPath(depart, stopby) + getPath(stopby, arrive)
+            path = getPath(depart, stopby) + getPath(stopby, arrive)
+            paths[(depart, arrive)] = path
+            path_set.add((depart, arrive))
+            return path
 
 
 for i in range(1, cities + 1):
@@ -53,6 +62,5 @@ for i in range(1, cities + 1):
         if path[0] == 0:
             print(0)
         else:
-            path += [j]
-            print(len(path), end=" ")
-            print(" ".join(map(str, path)))
+            print(len(path) + 1, end=" ")
+            print(" ".join(map(str, path + [j])))
