bc8c67af0afd56d1d8c855dac885f74aff0d9975
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-05-06 00:05:55 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -2,12 +2,11 @@ import sys
 from collections import deque
 
 N, K = map(int, sys.stdin.readline().rstrip().split())
+nlt = [i for i in range(1, N + 1)]
 queue = deque([i for i in range(1, N + 1)])
 result = []
-
-while len(result) < N:
-    for _ in range(K - 1):
-        queue.append(queue.popleft())
+for _ in range(N):
+    queue.rotate(1 - K)
     result.append(queue.popleft())
 
 print("<" + ", ".join(map(str, result)) + ">")
