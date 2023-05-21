c5117b815c451327d6d3bc173170e8b2b97cc9c0
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-05-21 23:54:57 +0900

    [Gold V] Title: 집합의 표현, Time: 308 ms, Memory: 77120 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,41 @@
+import sys
+
+sys.setrecursionlimit(10**6)
+# f = open("practice.txt", "r")
+# sentence = f.readline().rstrip()
+
+
+def findRoot(node):
+    if node == parent[node]:
+        return node
+    super = findRoot(parent[node])
+    parent[node] = super
+    return parent[node]
+
+
+def union(node1, node2):
+    node1 = findRoot(node1)
+    node2 = findRoot(node2)
+
+    if node1 == node2:
+        return
+    if node1 < node2:
+        parent[node2] = node1
+    else:
+        parent[node1] = node2
+
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+# N, M = map(int, f.readline().rstrip().split())
+
+parent = [i for i in range(N + 1)]
+
+for _ in range(M):
+    method, a, b = map(int, sys.stdin.readline().rstrip().split())
+    if method == 0:
+        union(a, b)
+    elif method == 1:
+        if findRoot(a) == findRoot(b):
+            print("YES")
+        else:
+            print("NO")
