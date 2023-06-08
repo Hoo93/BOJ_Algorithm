0185b2ebd259a7ecb38e6fb4ed80721fc9dbb60d
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-06-08 20:23:59 +0900

    [Silver II] Title: 사탕 게임, Time: 3872 ms, Memory: 31876 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -0,0 +1,76 @@
+import sys
+import copy
+
+size = int(sys.stdin.readline().rstrip())
+board = [ sys.stdin.readline().rstrip() for _ in range(size) ]
+
+# f = open("practice.txt","r")
+# size = int(f.readline().rstrip())
+# board = [ f.readline().rstrip() for _ in range(size) ]
+
+def findMaxLen(board):
+    result = 0 
+    
+    for i in range(size):
+        stack = [board[i][0]]
+        for j in range(1,size):
+            if not stack or stack[-1] == board[i][j]:
+                stack.append(board[i][j])
+            else:
+                result = max(result,len(stack))
+                stack = [board[i][j]]
+        result = max(result,len(stack))
+
+    for j in range(size):
+        stack = [board[0][j]]
+        for i in range(1,size):
+            if not stack or stack[-1] == board[i][j]:
+                stack.append(board[i][j])
+            else:
+                result = max(result,len(stack))
+                stack = [board[i][j]]
+        result = max(result,len(stack))
+    
+    return result
+
+def changeBoard(n_board,node_1,node_2):
+    # 행이 같은 경우
+    board = copy.deepcopy(n_board)
+    if node_1[0] == node_2[0]:
+        x1,x2 = min(node_1[1],node_2[1]),max(node_1[1],node_2[1])
+        y = node_1[0]
+        sentence = list(board[y])
+        sentence[x1],sentence[x2] = sentence[x2],sentence[x1]
+        board[y] = "".join(sentence)
+    else:
+        y1,y2 = min(node_1[0],node_2[0]),max(node_1[0],node_2[0])
+        x = node_1[1]
+
+        tmp = board[y1][x]
+
+        board[y1] = board[y1][:x] + board[y2][x] + board[y1][x+1:]
+        board[y2] = board[y2][:x] + tmp + board[y2][x+1:]
+    
+    return board
+
+if findMaxLen(board) == size:
+    print(size)
+else:
+    result = 0
+
+    for i in range(size):
+        for j in range(size-1):
+            if board[i][j] != board[i][j+1]:
+                result = max(result,findMaxLen(changeBoard(board,(i,j),(i,j+1))))
+
+            else:
+                continue
+    
+    for j in range(size):
+        for i in range(size-1):
+            if board[i][j] != board[i+1][j]:
+                result = max(result,findMaxLen(changeBoard(board,(i,j),(i+1,j))))
+            else:
+                continue
+    
+    print(result)
