581730c6fd8fbfe5cc5a95802307eba1019fed71
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2023-06-20 23:25:24 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 6200 ms, Memory: 304140 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,51 +1,29 @@
 import sys
 sys.setrecursionlimit(10**6)
-# def makeTree(node,parent):
-#     parents[node] = parent
 
-#     for v in graphs[node]:
-#         if v == parent:
-#             continue
-#         makeTree(v,node)
+def dfs(node):
+    visited[node] = True
 
-def getMinAdopter(node,parent,selected):
-    if dp[node][selected] != N:
-        return dp[node][selected]
-    
-    cnt = selected
-    
-    if selected:
-        for v in graphs[node]:
-            if v == parent:
-                continue
-            cnt += min(getMinAdopter(v,node,True),getMinAdopter(v,node,False))
-        
-    else:
-        for v in graphs[node]:
-            if v == parent:
-                continue
-            cnt += getMinAdopter(v,node,True)
-    
-    dp[node][selected] = cnt
-    return dp[node][selected]
+    for vertex in graphs[node]:
+        if visited[vertex]:
+            continue
+        dfs(vertex)
+        dp[node][1] += min(dp[vertex][0],dp[vertex][1])
+        dp[node][0] += dp[vertex][1]
 
+f = sys.stdin
+# f = open("practice.txt",'r')
 
-# f = open("practice.txt","r")
-# N = int(f.readline().rstrip())
-
-N = int(sys.stdin.readline().rstrip())
-
+N = int(f.readline().rstrip())
 graphs = [[] for _ in range(N+1)]
-dp = [[N,N] for _ in range(N+1)]
-parents = [-1 for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
+# populations = [0] + list(map(int,f.readline().rstrip().split()))
+dp = [[0,1] for i in range(N+1) ]
 
 for _ in range(N-1):
-    # n1,n2 = map(int,f.readline().rstrip().split())
-    n1,n2 = map(int,sys.stdin.readline().rstrip().split())
-    graphs[n1].append(n2)
+    n1,n2 = map(int,f.readline().rstrip().split())
     graphs[n2].append(n1)
+    graphs[n1].append(n2)
 
-getMinAdopter(1,-1,True)
-getMinAdopter(1,-1,False)
-
-print(min(dp[1]))
+dfs(N//2+1)
+print(min(dp[N//2+1]))
