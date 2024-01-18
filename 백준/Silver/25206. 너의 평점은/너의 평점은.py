c13c56aa98919955eb7cb5ae63f5d565eeef3afb
Hoo93 <117077999+Hoo93@users.noreply.github.com>
2024-01-19 01:23:38 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,20 +1,25 @@
 import sys
 
-pass_and_fail = set(['P','F'])
-result = 0
-total = 0
-dic = dict()
+num_sum = 0
+score = 0
 
-for grade,point in [('F',0),('D0',1.0),('D+',1.5),('C0',2.0),('C+',2.5),('B0',3.0),('B+',3.5),('A0',4.0),('A+',4.5)]:
-    dic[grade] = point
-
+g = {
+    'A+':4.5,
+    'A0':4.0,
+    'B+':3.5,
+    'B0':3.0,
+    'C+':2.5,
+    'C0':2.0,
+    'D+':1.5,
+    'D0':1.0,
+    'F':0.0
+}
 for _ in range(20):
-
-    a,b,c = sys.stdin.readline().rstrip().split()
-    if c == 'P':
+    _,num,grade = sys.stdin.readline().strip().split()
+    if grade == 'P':
         continue
-    
-    result += dic[c]*float(b)
-    total += float(b)
+    else:
+        score += float(num)*g[grade]
+        num_sum += float(num)
 
-print(round(result/total,5))
+print(round(score/num_sum,5))
