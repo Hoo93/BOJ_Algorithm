a2a7ca358e6dac7d807c9aa150c2b30582e4c6ba
Sang Hoo Park <117077999+Hoo93@users.noreply.github.com>
2024-09-21 00:04:29 +0900

    [Gold III] Title: 내리막 길, Time: 108 ms, Memory: 44292 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,44 +1,42 @@
 import sys
 
-row,col = map(int,sys.stdin.readline().rstrip().split())
-board = [] 
-delta = [(1,0),(-1,0),(0,1),(0,-1)]
+sys.setrecursionlimit(10000)
 
-dp = [ [-1 for _ in range(col)] for _ in range(row)]
-# dp의 초깃값을 -1로 설정한다. 들리지 않은 경우를 -1로 하기 위함
-# -1로 설정하는 것이 굉장히 중요하다!
-# 왜냐하면 0으로 설정하면 들렸는데 경로의 수가 0인 건지
-# 아니면 안들린건지 판별이 불가능하기 때문에
-# 들렸는데 경로의 수가 0인 경우에도 계속 방문을 하게 됨
-# 하지만 -1로 초기화 후에 들렸는데 경로가 0인 경우에는
-# dp 값이 0 이기 때문에 들렸다는 것을 확인이 가능하다
+# f = open('input.txt', 'r')
+# input = f.readline
+input = sys.stdin.readline
 
-for _ in range(row):
-    board.append(list(map(int,sys.stdin.readline().rstrip().split())))
+MAX = 10001
 
+
+N,M = map(int, input().split())
+board = [[MAX for _ in range(M+2)]]+[[MAX] + list(map(int, input().split())) + [MAX] for _ in range(N)]+[[MAX for _ in range(M+2)]]
+visited = [[-1 for _ in range(M+2)] for _ in range(N+2)]
+
+delta = [(-1,0),(1,0),(0,-1),(0,1)]
+
+# visited 체크
+# 더 낮은 숫자인지 확인
+# 더 낮은 숫자가 아닌 경우 return 0
+# 더 낮은 숫자인 경우 visited[y][x] += dfx(ny,nx)
 def dfs(y,x):
-    if y == row -1  and x == col - 1:
+    # 이미 방문한 경우 처리
+    if visited[y][x] != -1:
+        return visited[y][x]
+    # 종점에 도착한 경우 처리
+    if y == N and x == M:
         return 1
-        # 도착 지점에 도달한 경우 경로의 수를 1 추가
-    
-    # 이미 들렀던 경우에는
-    # (y,x) 에서(row,col) 까지 가는 경로의 수인
-    # dp[y][x]를 호출 하고 DFS 종료
-    if dp[y][x] != -1:
-        return dp[y][x]
-    
-    
-    dp[y][x] = 0
-    # 0으로 바꿔서 들렸다고 표시하고 row,col 까지 가는 경로의 수를 0으로 초기화
+    # 처음 도착한 경우 초기화
+    visited[y][x] = 0
+
     for dy,dx in delta:
         ny,nx = y+dy,x+dx
-
-        if  ny == -1 or ny == row or nx == -1 or nx == col :
+        if board[ny][nx] >= board[y][x]:
             continue
-        if board[y][x] <= board[ny][nx]:
-            continue
-        dp[y][x] += dfs(ny,nx)
+        visited[y][x] += dfs(ny,nx)
 
-    return dp[y][x]
+    return visited[y][x]
 
-print(dfs(0,0))
+dfs(1,1)
+
+print(visited[1][1])
